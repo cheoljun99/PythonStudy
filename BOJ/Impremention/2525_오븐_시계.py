c8500fcef2f3d101071550e329d7f4730f793b64
cheoljun99 <cheoljun99@naver.com>
2024-06-25 14:41:14 +0900

    [feat]: 2024.06.25-2

---
+++ BOJ/Impremention/2525_오븐_시계.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+A, B = map(int, input().split())  # map 함수란 주어진 함수와 반복 가능한 객체를 인자로 받아 해당 함수가 모든 요소에 적용된 결과를 반환하는 함수임
+C = int(input())
+
+total = B + C
+
+share = total // 60  # 정수 나눗셈
+remainder = total % 60
+
+A=A+share
+B=remainder
+A= A%24
+print(A, B)
